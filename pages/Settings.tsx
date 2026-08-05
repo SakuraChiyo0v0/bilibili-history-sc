dce1bb873752cab95e8b2858766ad39e441b68f6
SakuraChiyo0v0 <3296299414@qq.com>
2026-08-05 11:37:56 +0800

    feat(settings): 侧边栏菜单管理新增隐藏赞赏选项

--- pages/Settings.tsx
+++ pages/Settings.tsx
@@ -453,7 +453,7 @@ const Settings = () => {
               onChange={handleHideUserInfoChange}
             />
 
-            {["收藏夹", "合集", "听歌", "云同步", "WebDAV", "关于", "反馈"].map((title) => (
+            {["收藏夹", "合集", "听歌", "云同步", "WebDAV", "关于", "反馈", "赞赏"].map((title) => (
               <Checkbox
                 key={title}
                 label={`隐藏${title}`}
